1e886bb0b826faa0320586e6567d7b3225210149
DamianPyCoder <119323521+DamianPyCoder@users.noreply.github.com>
2022-12-04 16:21:53 +0100

    Add files via upload

---
+++ web_3/script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#fa6a23","#fb7938","#ff945e","#fbac85","#f9cbb3"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
+
+
